377026445f188ddbb94c51458ee40ddc0ea5d02f
Warren Green <s.green.warren@gmail.com>
2013-12-25 21:54:13 -0500

    Technically Multithreaded
    Multithreaded but does not support Keep-Alive sessions so every request
    requires two RTT

--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -14,14 +15,15 @@ import javax.imageio.ImageIO;
 
 public class Server {
 	
-	private final String CONTENT404 = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1>The requested resource could not be found but may be available again in the future.</html>";
-	private final String CONTENT501 = "<html><head><title>501 Not Implemented</title></head><body><h1>501 Not Implemented</h1>The server either does not recognise the request method, or it lacks the ability to fulfill the request.</body></html>";
+	protected final static String CONTENT404 = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1>The requested resource could not be found but may be available again in the future.</html>";
+	protected final static String CONTENT501 = "<html><head><title>501 Not Implemented</title></head><body><h1>501 Not Implemented</h1>The server either does not recognise the request method, or it lacks the ability to fulfill the request.</body></html>";
 
-	private final String STRING200 = "OK";
-	private final String STRING404 = "Not Found";
-	private final String STRING501 = "Not Implemented";
+	protected final static String STRING200 = "OK";
+	protected final static String STRING404 = "Not Found";
+	protected final static String STRING501 = "Not Implemented";
 	
 	private int port;
+	protected static volatile boolean running = true;
 		
 	public Server(int port){
 		this.port = port;
@@ -31,11 +33,21 @@ public class Server {
 		
 		Socket c_sock = null;
 		ServerSocket s_sock = null;
-		
+		ArrayList<Thread> clients = new ArrayList<Thread>();
+
 		//Open ports.
 		try{
 			s_sock = new ServerSocket(port);
-			c_sock = s_sock.accept();
+			
+			while(running){
+				Runnable r = new Worker(s_sock.accept());
+				clients.add(new Thread(r));
+				clients.get(clients.size()-1).start();
+			}
+			
+			for(Thread th: clients){
+				th.join();
+			}
 			
 		}catch(IOException ex){
 			System.out.println("Port already in use.");
@@ -46,144 +58,27 @@ public class Server {
 		}catch(IllegalArgumentException ex){
 			System.out.println("Invalid port number. Range: [1025, 65535]");
 			ex.printStackTrace();
-		}
-		
-		try{
-			while(true){
-				BufferedOutputStream out = new BufferedOutputStream(c_sock.getOutputStream());
-				
-				HashMap<String, String> header = parse(c_sock);
-				
-				sendResponse(header, out);
-				
-				if(header.get("connection") == null || header.get("connection").compareToIgnoreCase("keep-alive") != 0)
-					break;
-				
-				c_sock = s_sock.accept();
-			}
-			
-		}catch(IOException ex){
-			//TODO: Fill Exceptions
-			ex.printStackTrace();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 
 	}
 	
-	public void sendResponse(HashMap<String, String> header, BufferedOutputStream out) throws IOException{
-		int response = 200;
-		String respString = STRING200;
-		String contentType = "text/html";
-		byte[] content = new byte[5];
-		String connection = "close";
-		
-		if(header.get("file") == null){
-			response = 501;
-			respString = STRING501;
-			content = CONTENT501.getBytes();
-		}else if(! new File(header.get("file")).exists()){
-			response = 404;
-			respString = STRING404;
-			content = CONTENT404.getBytes();
-		}else if(new File(header.get("file")).isDirectory()){
-			content = generateIndex(header.get("file")).getBytes();
-		}else{
-			contentType = getFileType(header.get("file"));
-			if(contentType.compareTo("application/octet-stream")!= 0){
-				content = readImage(header.get("file"));
-			}
-		}
-		
-		if(header.get("connection") != null && header.get("connection").compareToIgnoreCase("keep-alive") == 0){
-			connection = "keep-alive";
-		}
-		
-		String headRet = String.format("HTTP/1.1 %d %s\r\nQuickServ\r\nMIME-version: 1.0\r\nContent-type: %s\r\nContent-Length: %d\r\nConnection: %s\r\n\r\n", response, respString, contentType, content.length, connection);
 	
-		out.write(headRet.getBytes());
-		out.write(content);
-		
-		out.close();
-	}
-	
-	public byte[] readImage(String filename){
-		
-		byte[] imgBytes = null;
-		try {
-			BufferedImage img = ImageIO.read(new File(filename));
-		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		    ImageIO.write( img, "jpg", baos );
-		    baos.flush();
-		    imgBytes = baos.toByteArray();
-		    baos.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
-		
-		return imgBytes;
-	}
-	
-	public String generateIndex(String filename){
-		File file = new File(filename);
-		String ret = "<html><body><h1>QuickServ</h1><br />";
-		String path = "";
-		
-		if(filename.compareTo("./") != 0 ){
-			path = filename.substring(1) +"/";
-		}
-		
-		for(File child: file.listFiles()){
-			ret = ret + String.format("<a href='%s'>%s</a><br />", path + child.getName(), child.getName());
-		}
-		
-		ret.concat("</body></html>");
-		return ret;
-	}
-	
-	public HashMap<String, String> parse(Socket c_sock){
-		HashMap<String, String> header = new HashMap<String, String>();
-		try{
-			BufferedReader in  = new BufferedReader(
-					new InputStreamReader(c_sock.getInputStream()));
-			
-			String inputLine;
-			
-			if((inputLine = in.readLine()) != null){
-				header.put("status", inputLine);
-				String file = inputLine.split(" ")[1];
-				if(file.indexOf("..") >=0 ) file = null;
-				header.put("file", "."+file);
-			}
-			
-			while((inputLine = in.readLine())!= null && !inputLine.isEmpty()){
-				int sep = inputLine.indexOf(":");
-				if(sep >0){
-					header.put(inputLine.substring(0, sep).toLowerCase(), inputLine.substring(sep+2, inputLine.length()));
-				}
-			}
-			
-		}catch(IOException ex){
-			//TODO: Fill Exceptions
-			ex.printStackTrace();
-		}
-		
-		return header;
-	}
-	
-	public String getFileType(String file){
-		String contentType = "";
-		
-		if(file.indexOf(".jpeg") >= 0 || file.indexOf(".jpg") >=0 ){
-			contentType = "image/jpeg";
-		}else if(file.indexOf(".png") >= 0){
-			contentType = "image/png";
-		}else{
-			contentType = "application/octet-stream";
-		}
-		
-		return contentType;
-	}
 	
 	public static void main(String[] args){
+		/*final Thread mainThread = Thread.currentThread();
+		Runtime.getRuntime().addShutdownHook(new Thread(){
+			public void run(){
+				running = false;
+				try {
+					mainThread.join();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		});*/
+		
 		if(args.length < 1){
 			System.out.println("Usage: QuickServ.jar [port number]");
 			System.exit(1);
---
+++ src/Worker.java
@@ -0,0 +1,164 @@
+import java.awt.image.BufferedImage;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.imageio.ImageIO;
+
+
+public class Worker implements Runnable{
+	private Socket c_sock;
+	
+	public Worker(Socket c_sock){
+		this.c_sock = c_sock;
+	}
+	
+	@Override
+	public void run() {
+		try{
+			BufferedOutputStream out = new BufferedOutputStream(c_sock.getOutputStream());
+			BufferedReader in  = new BufferedReader(new InputStreamReader(c_sock.getInputStream()));
+			c_sock.setKeepAlive(true);
+			
+						
+			//while(Server.running && !c_sock.isClosed() && c_sock.getKeepAlive()){
+				HashMap<String, String> header = parse(in);
+				
+				sendResponse(header, out);
+				
+				if(header.get("connection") == null || header.get("connection").compareToIgnoreCase("keep-alive") != 0){
+					c_sock.setKeepAlive(false);
+				}
+				
+			//}
+			out.close();
+			in.close();
+
+			
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+
+		return;
+		
+	}
+	
+	
+	public void sendResponse(HashMap<String, String> header, BufferedOutputStream out) throws IOException{
+		int response = 200;
+		String respString = Server.STRING200;
+		String contentType = "text/html";
+		byte[] content = new byte[5];
+		String connection = "close";
+		
+		
+		if(header.get("file") == null){
+			response = 501;
+			respString = Server.STRING501;
+			content = Server.CONTENT501.getBytes();
+		}else if(! new File(header.get("file")).exists()){
+			response = 404;
+			respString = Server.STRING404;
+			content = Server.CONTENT404.getBytes();
+		}else if(new File(header.get("file")).isDirectory()){
+			content = generateIndex(header.get("file")).getBytes();
+		}else{
+			contentType = getFileType(header.get("file"));
+			if(contentType.compareTo("application/octet-stream")!= 0){
+				content = readImage(header.get("file"));
+			}
+		}
+		
+		if(header.get("connection") != null && header.get("connection").compareToIgnoreCase("keep-alive") == 0){
+			connection = "keep-alive";
+		}
+		
+		String headRet = String.format("HTTP/1.1 %d %s\r\nQuickServ\r\nMIME-version: 1.0\r\nContent-type: %s\r\nContent-Length: %d\r\nConnection: %s\r\n\r\n", response, respString, contentType, content.length, connection);
+	
+		out.write(headRet.getBytes());
+		out.write(content);
+	}
+	
+	public byte[] readImage(String filename){
+		
+		byte[] imgBytes = null;
+		try {
+			BufferedImage img = ImageIO.read(new File(filename));
+		    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		    ImageIO.write( img, "jpg", baos );
+		    baos.flush();
+		    imgBytes = baos.toByteArray();
+		    baos.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		return imgBytes;
+	}
+	
+	public String generateIndex(String filename){
+		File file = new File(filename);
+		String ret = "<html><body><h1>QuickServ</h1><br />";
+		String path = "";
+		
+		if(filename.compareTo("./") != 0 ){
+			path = filename.substring(1) +"/";
+		}
+		
+		for(File child: file.listFiles()){
+			ret = ret + String.format("<a href='%s'>%s</a><br />", path + child.getName(), child.getName());
+		}
+		
+		ret.concat("</body></html>");
+		return ret;
+	}
+	
+	public HashMap<String, String> parse(BufferedReader in){
+		HashMap<String, String> header = new HashMap<String, String>();
+		try{
+
+			String inputLine;
+			
+			if((inputLine = in.readLine()) != null){
+				header.put("status", inputLine);
+				String file = inputLine.split(" ")[1];
+				if(file.indexOf("..") >=0 ) file = null;
+				header.put("file", "."+file);
+			}
+			
+			while((inputLine = in.readLine())!= null && !inputLine.isEmpty()){
+				int sep = inputLine.indexOf(":");
+				if(sep >0){
+					header.put(inputLine.substring(0, sep).toLowerCase(), inputLine.substring(sep+2, inputLine.length()));
+				}
+			}
+			
+		}catch(IOException ex){
+			//TODO: Fill Exceptions
+			ex.printStackTrace();
+		}
+		
+		return header;
+	}
+	
+	public String getFileType(String file){
+		String contentType = "";
+		
+		if(file.indexOf(".jpeg") >= 0 || file.indexOf(".jpg") >=0 ){
+			contentType = "image/jpeg";
+		}else if(file.indexOf(".png") >= 0){
+			contentType = "image/png";
+		}else{
+			contentType = "application/octet-stream";
+		}
+		
+		return contentType;
+	}
+
+}
